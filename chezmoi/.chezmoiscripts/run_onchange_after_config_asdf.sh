ca4a08ac709f656fb63d9e8ccc6315fe95d5fab6
Angelo Tata <angelo.tata@gmail.com>
2022-05-27 15:48:35 +0100

    Make asdf initialisation script more robust

--- chezmoi/.chezmoiscripts/run_onchange_after_config_asdf.sh
+++ chezmoi/.chezmoiscripts/run_onchange_after_config_asdf.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -eufo pipefail
 
-asdf plugin add direnv https://github.com/asdf-community/asdf-direnv.git
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin-add python https://github.com/danhper/asdf-python.git
-asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
-asdf plugin-add asdf-golang https://github.com/kennyp/asdf-golang.git
+asdf plugin add direnv https://github.com/asdf-community/asdf-direnv.git || true
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git || true
+asdf plugin-add python https://github.com/danhper/asdf-python.git || true
+asdf plugin-add rust https://github.com/code-lever/asdf-rust.git || true
+asdf plugin-add asdf-golang https://github.com/kennyp/asdf-golang.git || true
 
 asdf install direnv latest
 asdf global direnv latest
